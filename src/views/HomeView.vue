307330f0b1840627b2d664e8cfd543338755d694
c-arena <carlostata.ca@gmail.com>
2024-06-10 05:12:20 -0400

    implemented home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import bgPattern from '@/assets/polka-dots.svg'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="container w-screen h-screen grid content-center bg-fixed text-center" :style="{backgroundImage: `url(${bgPattern})`}">
+    <h1 class="text-5xl font-bold">Telefunken</h1>
+    <p class="py-6">Tablero de Puntajes</p>
+    <RouterLink to="/new-game" class="btn btn-primary w-fit mx-auto">Iniciar Juego Nuevo</RouterLink>
   </main>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui'
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      
+    }
   },
-  plugins: [],
+  plugins: [daisyui],
+  daisyui: {
+    themes: ['light', 'dark', 'cupcake']
+  }
 }
-
